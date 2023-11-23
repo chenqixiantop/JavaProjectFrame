d34f4dec7f55288ff326cc9d749506c2ff146e74
qixian.chen <534868767@qq.com>
2023-11-23 13:30:13 +0800

    提交rabbitmq生产者代码

--- cqx-examples/rabbitmq-springboot/src/main/java/com/chenqixian/cloud/examples/rabbitmq/RabbitmqApplication.java
+++ cqx-examples/rabbitmq-springboot/src/main/java/com/chenqixian/cloud/examples/rabbitmq/RabbitmqApplication.java
@@ -3,6 +3,9 @@ package com.chenqixian.cloud.examples.rabbitmq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 53486
+ */
 @SpringBootApplication
 public class RabbitmqApplication {
 
